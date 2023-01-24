7053c7817f72c8dc6399d4155778ca04243c5c7b
Jyles Lulham <jyles@hotmail.com.au>
2023-01-24 14:10:57 +1100

    Created a functioning round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ROCK PAPER SCISSORS GAME
 //getComputerChoice randomly from the 3 options Rock, Paper & Scissors.
 let getComputerChoice = Math.floor(Math.random() * 3);
-console.log(getComputerChoice);
+console.log(`Computer random number = ${getComputerChoice}`);
 
 function computerSelection() {
     if (getComputerChoice === 0) {
@@ -13,7 +13,43 @@ function computerSelection() {
     }
 }
 
-console.log(computerSelection());
-// getUserChoice from a predefined list of Rock, Paper & Scissors
+console.log(`Which converts into equally ${computerSelection()}`);
+
+// define the players selection
+
+let playerSelection = prompt("Rock, Paper or Scissors?");
+
+playerSelection = playerSelection.toUpperCase();
+console.log(`Our player types ${playerSelection}`);
+
 // Check if player option beats the computer option
+
+// PAPER beats ROCK
+// SCISSORS beats PAPER
+// ROCK beats SCISSORS
+
+
+function playRound(playerSelection, getComputerChoice) {
+    if (playerSelection === 'PAPER' && getComputerChoice === 'ROCK') {
+        return 'Player wins, as Paper beats Rock.';
+    } else if (playerSelection === 'SCISSORS' && getComputerChoice === 'PAPER') {
+       return 'Player wins, as Scissors beats Paper.';
+    } else if (playerSelection === 'ROCK' && getComputerChoice === 'SCISSORS') {
+        return 'Player wins, as Rock beats Scissors.';
+    } else if (playerSelection === getComputerChoice) {
+        return 'Draw.';
+    } else {
+        return 'Player loses.'
+    }
+}
+
 // Return the result
+
+console.log(`Our Player chooses ${playerSelection} and the Computer choose ${getComputerChoice}. ${playRound(playerSelection, getComputerChoice)}`);
+
+
+//We to make a 5 round game
+// Create a loop that lasts for 5 rounds
+// Keep player score and computer score
+// Declare a winner based off of who has the highest score after 5 rounds
+// Restart the game after the 5 rounds has been played
